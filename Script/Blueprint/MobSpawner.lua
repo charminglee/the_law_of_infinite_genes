9fd2c1f0695cbbe200ec65b3e70604fad3540c1a
Nuoyan <1279735247@qq.com>
2026-05-29 17:49:39 +0800

    同步进度

---
+++ Script/Blueprint/MobSpawner.lua
@@ -0,0 +1,19 @@
+local MobSpawner = {}
+ 
+--[[
+function MobSpawner:ReceiveBeginPlay()
+    MobSpawner.SuperClass.ReceiveBeginPlay(self)
+end
+--]]
+
+--[[
+function MobSpawner:OnMobSpawn(MobPawn)
+    
+end
+--]]
+
+function MobSpawner:CustomSpawnMob()
+    
+end
+
+return MobSpawner
---
+++ Script/Blueprint/Prefabs/Monsters/Infected.lua
@@ -0,0 +1,137 @@
+---@class Infected_C:BP_UGC_GenericMobPawn_Base_C
+---@field HitBox UCapsuleComponent
+--Edit Below--
+local Infected = {}
+
+-- function Infected:ReceiveBeginPlay()
+--     Infected.SuperClass.ReceiveBeginPlay(self)
+-- end
+
+-- function Infected:ReceiveTick(DeltaTime)
+--     Infected.SuperClass.ReceiveTick(self, DeltaTime)
+-- end
+
+-- function Infected:ReceiveEndPlay()
+--     Infected.SuperClass.ReceiveEndPlay(self) 
+-- end
+
+-- function Infected:GetReplicatedProperties()
+--     return
+-- end 
+
+-- ---受击前置事件
+-- ---生效范围：服务器
+-- ---@param Damage float 伤害值
+-- ---@param EventInstigator AController 伤害来源的Controller
+-- ---@param DamageCauser AActor 伤害来源
+-- ---@param DamageContext FGameMagnitudeContext  伤害上下文
+-- function Infected:PreTakeDamageEvent(Damage, EventInstigator, DamageCauser, DamageContext)
+     
+-- end
+
+-- ---受击后置事件
+-- ---生效范围：服务器
+-- ---@param Damage float 伤害值
+-- ---@param EventInstigator AController 伤害来源的Controller
+-- ---@param DamageCauser AActor 伤害来源
+-- ---@param DamageContext FGameMagnitudeContext  伤害上下文
+-- function Infected:PostTakeDamageEvent(Damage, EventInstigator, DamageCauser, DamageContext)
+    
+-- end
+
+-- ---受击前置伤害修改
+-- ---生效范围：服务器
+-- ---@param Damage float 伤害值
+-- ---@param EventInstigator AController 伤害来源的Controller
+-- ---@param DamageCauser AActor 伤害来源
+-- ---@param DamageContext FGameMagnitudeContext  伤害上下文
+-- ---@return float 修改后的伤害值
+-- function Infected:PreOverrideDamage(Damage, EventInstigator, DamageCauser, DamageContext)
+--     return Damage
+-- end
+
+-- ---受击后置伤害修改
+-- ---生效范围：服务器
+-- ---@param Damage float 伤害值
+-- ---@param EventInstigator AController 伤害来源的Controller
+-- ---@param DamageCauser AActor 伤害来源
+-- ---@param DamageContext FGameMagnitudeContext  伤害上下文
+-- ---@return float 修改后的伤害值
+-- function Infected:PostOverrideDamage(Damage, EventInstigator, DamageCauser, DamageContext)
+--     return Damage
+-- end
+
+---角色死亡事件
+---生效范围：服务器&客户端
+-- ---@param Damage float 伤害值
+-- ---@param EventInstigator AController 伤害来源的Controller
+-- ---@param DamageCauser AActor 伤害来源
+-- ---@param FDamageEvent DamageEvent 伤害事件
+-- ---@param DamageTypeID int32 伤害类型
+-- function Infected:BPDie(KillingDamage, EventInstigator, DamageCauser, DamageEvent, DamageTypeID)
+--     if self:HasAuthority() then
+--         -- 只有服务端才可以掉落
+--         self.UGCPresetCommonDropItemComponent:StartDrop(self, EventInstigator, {})
+--     end
+-- end
+
+-- ---状态进入事件
+-- ---生效范围：服务器&客户端
+-- ---@param DynamicState FGameplayTag 进入的状态
+-- function Infected:OnEnterTagState_BP(DynamicState)
+--     local Tag = BlueprintGameplayTagLibrary.GetTagName(DynamicState)
+--     ugcprint('OnEnterTagState_BP: ' .. Tag)
+-- end
+
+-- ---状态退出事件
+-- ---生效范围：服务器&客户端
+-- ---@param DynamicState FGameplayTag 退出的状态
+-- function Infected:OnLeaveTagState_BP(DynamicState)
+--     local Tag = BlueprintGameplayTagLibrary.GetTagName(DynamicState)
+--     ugcprint('OnLeaveTagState_BP: ' .. Tag)
+-- end
+
+-- ---状态打断事件
+-- ---生效范围：服务器&客户端
+-- ---@param DynamicState FGameplayTag 打断的状态
+-- function Infected:OnInterruptTagState_BP(DynamicState)
+--     local Tag = BlueprintGameplayTagLibrary.GetTagName(DynamicState)
+--     ugcprint('OnInterruptTagState_BP' .. Tag)
+-- end
+
+-- ---行为树消息
+-- ---生效范围：服务器
+-- ---@param NotifyMsg string 消息
+-- function Infected:OnBehaviorNotify_BP(NotifyMsg)
+--     ugcprint('OnBehaviorNotify_BP: ' .. NotifyMsg)
+-- end
+
+-- ---怪物的目标发生变化事件
+-- ---生效范围：服务器&客户端
+-- ---@param NewTarget AActor 新目标
+-- ---@param OldTarget AActor 旧目标
+-- function Infected:OnTargetChange_BP(NewTarget, OldTarget)
+    
+-- end
+
+-- [Editor Generated Lua] function define Begin:
+function Infected:LuaInit()
+	if self.bInitDoOnce then
+		return;
+	end
+	self.bInitDoOnce = true;
+	-- [Editor Generated Lua] BindingProperty Begin:
+	-- [Editor Generated Lua] BindingProperty End;
+	
+	-- [Editor Generated Lua] BindingEvent Begin:
+	-- self.UGCPresetCommonDropItemComponent.OnDropItem:Add(self.UGCPresetCommonDropItemComponent_OnDropItem, self);
+	-- [Editor Generated Lua] BindingEvent End;
+end
+
+-- function Infected:UGCPresetCommonDropItemComponent_OnDropItem(ItemActor)
+-- 	return nil;
+-- end
+
+-- [Editor Generated Lua] function define End;
+
+return Infected
