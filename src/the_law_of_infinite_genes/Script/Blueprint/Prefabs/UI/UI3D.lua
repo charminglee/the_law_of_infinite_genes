8bd523d04ef33c4391fa9b769a14708de23320e8
TEFuc0h1 <14045800+TEFuc0h1@user.noreply.gitee.com>
2026-05-31 18:17:07 +0800

    加入赛季称号，充值称号，财富称号。调整称号配置

--- src/the_law_of_infinite_genes/Script/Blueprint/Prefabs/UI/UI3D.lua
+++ src/the_law_of_infinite_genes/Script/Blueprint/Prefabs/UI/UI3D.lua
@@ -1,13 +1,15 @@
 ---@class UI3D_C:UUserWidget
 ---@field Icon UImage
+---@field Title UTextBlock
 --Edit Below--
 local UI3D = { bInitDoOnce = false } 
 
---[==[ Construct
+
 function UI3D:Construct()
+	self:LuaInit();
 	
 end
--- Construct ]==]
+
 
 -- function UI3D:Tick(MyGeometry, InDeltaTime)
 
@@ -16,5 +18,34 @@ end
 -- function UI3D:Destruct()
 
 -- end
+
+-- [Editor Generated Lua] function define Begin:
+function UI3D:LuaInit()
+	if self.bInitDoOnce then
+		return;
+	end
+	self.bInitDoOnce = true;
+	-- [Editor Generated Lua] BindingProperty Begin:
+	self.Title:BindingProperty("Text", self.Title_Text, self);
+	self.Icon:BindingProperty("Brush", self.Icon_Brush, self);
+	-- [Editor Generated Lua] BindingProperty End;
+	
+	-- [Editor Generated Lua] BindingEvent Begin:
+	-- [Editor Generated Lua] BindingEvent End;
+end
+
+function UI3D:Title_Text(ReturnValue)
+	return "略有盈余";
+end
+
+function UI3D:Icon_Brush(ReturnValue)
+	return {
+        Brush = {
+            ResourceObject=UE.LoadObject("MaterialInstanceConstant'/the_law_of_infinite_genes/Asset/Texture/Titles/WealthTitle_1.WealthTitle_1'")
+        }
+    };
+end
+
+-- [Editor Generated Lua] function define End;
 
 return UI3D
