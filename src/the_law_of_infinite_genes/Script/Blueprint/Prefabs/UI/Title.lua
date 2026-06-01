dce0588d9bafadb8bb5cf8f0bf3c2e889864d4d5
TEFuc0h1 <14045800+TEFuc0h1@user.noreply.gitee.com>
2026-06-01 17:45:50 +0800

    更改称号加载方式

---
+++ src/the_law_of_infinite_genes/Script/Blueprint/Prefabs/UI/Title.lua
@@ -0,0 +1,20 @@
+---@class Title_C:ObjectPositionWidget
+---@field Icon UImage
+--Edit Below--
+local Title = { bInitDoOnce = false } 
+
+--[==[ Construct
+function Title:Construct()
+	
+end
+-- Construct ]==]
+
+-- function Title:Tick(MyGeometry, InDeltaTime)
+
+-- end
+
+-- function Title:Destruct()
+
+-- end
+
+return Title
