1e00aab58dc0f771cb14d813b38eead9945aa4e4
TEFuc0h1 <14045800+TEFuc0h1@user.noreply.gitee.com>
2026-06-08 19:47:57 +0800

    加入成就相关界面图以及蓝图脚本文件

---
+++ src/the_law_of_infinite_genes/Script/Blueprint/Prefabs/UI/Achievement/ACHVMain.lua
@@ -0,0 +1,19 @@
+---@class ACHVMain_C:UUserWidget
+--Edit Below--
+local ACHVMain = { bInitDoOnce = false } 
+
+--[==[ Construct
+function ACHVMain:Construct()
+	
+end
+-- Construct ]==]
+
+-- function ACHVMain:Tick(MyGeometry, InDeltaTime)
+
+-- end
+
+-- function ACHVMain:Destruct()
+
+-- end
+
+return ACHVMain
