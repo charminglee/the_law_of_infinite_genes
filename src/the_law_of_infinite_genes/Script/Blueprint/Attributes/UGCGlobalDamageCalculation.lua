088700d2dcdfaafef2fe9fe6f461129f062f3e8c
Nuoyan <1279735247@qq.com>
2026-06-19 13:39:34 +0800

    将三种怪物伤害加成加入到全局伤害公式

--- src/the_law_of_infinite_genes/Script/Blueprint/Attributes/UGCGlobalDamageCalculation.lua
+++ src/the_law_of_infinite_genes/Script/Blueprint/Attributes/UGCGlobalDamageCalculation.lua
@@ -20,42 +20,39 @@ function UGCGlobalDamageCalculation:GetCalculationResult(context, extraResult)
     end
     
     local instigator                = UGCAttributeSystem.GetInstigatorFromContext(context):K2_GetPawn()
-    local instiBreakDefenceRatio    = UGCAttributeSystem.GetGameAttributeValue(instigator, UGCCustomGameAttributeType.UGCAttributeGroup_Character_BreakDefenceRatio)
-    local instiCritChance           = UGCAttributeSystem.GetGameAttributeValue(instigator, UGCCustomGameAttributeType.UGCAttributeGroup_Character_CritChance)
-    local instiCritDamageBoost      = UGCAttributeSystem.GetGameAttributeValue(instigator, UGCCustomGameAttributeType.UGCAttributeGroup_Character_CritDamageBoost)
-    local instiDamageBoost          = UGCAttributeSystem.GetGameAttributeValue(instigator, UGCCustomGameAttributeType.UGCAttributeGroup_Character_DamageBoost)
-    local instiLv                   = UGCAttributeSystem.GetGameAttributeValue(instigator, UGCCustomGameAttributeType.UGCAttributeGroup_Character_Level)
+    local breakDefenceRatio         = UGCAttributeSystem.GetGameAttributeValue(instigator, UGCCustomGameAttributeType.UGCAttributeGroup_Character_BreakDefenceRatio)
+    local critChance                = UGCAttributeSystem.GetGameAttributeValue(instigator, UGCCustomGameAttributeType.UGCAttributeGroup_Character_CritChance)
+    local critDamageBoost           = UGCAttributeSystem.GetGameAttributeValue(instigator, UGCCustomGameAttributeType.UGCAttributeGroup_Character_CritDamageBoost)
+    local damageBoost               = UGCAttributeSystem.GetGameAttributeValue(instigator, UGCCustomGameAttributeType.UGCAttributeGroup_Character_DamageBoost)
+    local normalMonsterDamageBoost  = UGCAttributeSystem.GetGameAttributeValue(instigator, UGCCustomGameAttributeType.UGCAttributeGroup_Character_NormalMonsterDamageBoost)
+    local eliteDamageBoost          = UGCAttributeSystem.GetGameAttributeValue(instigator, UGCCustomGameAttributeType.UGCAttributeGroup_Character_EliteMonsterDamageBoost)
+    local bossDamageBoost           = UGCAttributeSystem.GetGameAttributeValue(instigator, UGCCustomGameAttributeType.UGCAttributeGroup_Character_BossDamageBoost)
     
-    local victim            = UGCAttributeSystem.GetVictimFromContext(context)
-    local vicDefence        = UGCAttributeSystem.GetGameAttributeValue(victim, UGCCustomGameAttributeType.UGCAttributeGroup_Character_Defence)
-    local vicDefenceBoost   = UGCAttributeSystem.GetGameAttributeValue(victim, UGCCustomGameAttributeType.UGCAttributeGroup_Character_DefenseBoost)
-    local vicLv             = UGCAttributeSystem.GetGameAttributeValue(victim, UGCCustomGameAttributeType.UGCAttributeGroup_Character_Level)
+    local victim        = UGCAttributeSystem.GetVictimFromContext(context)
+    local defence       = UGCAttributeSystem.GetGameAttributeValue(victim, UGCCustomGameAttributeType.UGCAttributeGroup_Character_Defence)
+    local defenceBoost  = UGCAttributeSystem.GetGameAttributeValue(victim, UGCCustomGameAttributeType.UGCAttributeGroup_Character_DefenseBoost)
     
-    -- A = instigator, B = victim
-    -- B.总防御 = B.防御 * (1 + B.防御加成比例)
-    -- 防御减伤比 = B.总防御 * (1 - A.防御穿透百分比) / (B.总防御 + K)
-    local totalDefence = vicDefence * (1 + vicDefenceBoost)
-    local damageDecreace = totalDefence * (1 - instiBreakDefenceRatio) / (totalDefence + Config.Damage.DefenceK)
-
-    -- 暴击
-    local isCrit = (math.random() <= instiCritChance)
+    -- 暴击区
+    local isCrit = (math.random() <= critChance)
     local critBoost = 0
     if isCrit then
-        critBoost = instiCritDamageBoost
+        critBoost = critDamageBoost
         local critTag = UGCGameplayTagSystem.RequestGameplayTag("UGC.Damage.Type.Critical")
         if critTag then
             extraResult.ResultTags:Add(critTag)
         end
     end
 
-    -- 猎尸狂涌：玩家减免受到怪物的所有伤害
-    -- if SpecialEventManager.currEvent == SpecialEvent.CorpseHuntingSurge and not instigator:IsPlayerController() then
-    --     local mul = 1 - Config.SpecialEvent[SpecialEvent.CorpseHuntingSurge].DefenseBuff
-    --     damage = damage * mul
-    -- end
-    
+    -- 增伤区
+    local totalDamageBoost = damageBoost + normalMonsterDamageBoost + eliteDamageBoost + bossDamageBoost
+
+    -- 总防御 = 防御 * (1 + 防御加成比)
+    -- 防御减伤比 = 总防御 * (1 - 防御穿透比) / (总防御 + K)
+    local totalDefence = defence * (1 + defenceBoost)
+    local damageDecreace = totalDefence * (1 - breakDefenceRatio) / (totalDefence + Config.Damage.DefenceK)
+
     -- 最终伤害
-    local finalDamage = damage * (1 + critBoost) * (1 + instiDamageBoost) * (1 - damageDecreace)
+    local finalDamage = damage * (1 + critBoost) * (1 + totalDamageBoost) * (1 - damageDecreace)
     return finalDamage, extraResult
 end
 
