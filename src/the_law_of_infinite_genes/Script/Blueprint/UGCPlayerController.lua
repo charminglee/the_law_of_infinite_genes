411e7f036bf6e4f5e08a5da21c9389ca136ee142
Nuoyan <1279735247@qq.com>
2026-06-30 17:41:33 +0800

    编辑器更新

--- src/the_law_of_infinite_genes/Script/Blueprint/UGCPlayerController.lua
+++ src/the_law_of_infinite_genes/Script/Blueprint/UGCPlayerController.lua
@@ -3,7 +3,7 @@
 ---@field GlobalEventComponent GlobalEventComponent_C
 ---@field GachaComponent GachaComponent_C
 ---@field FightComponent FightComponent_C
----@field ACHVComponent CHVComponent_C
+---@field ACHVComponent ACHVComponent_C
 ---@field StoreComponent StoreComponent_C
 ---@field HomeComponent HomeComponent_C
 ---@field RankingListComponent RankingListComponent_C
