8cb72ce743e814207957dfe5a7377a652843977d
cc <3331057238@qq.com>
2026-07-29 22:40:40 +0800

    更新背包系统/物品

--- src/the_law_of_infinite_genes/Script/Blueprint/Prefabs/UI/Lobby/Store/StoreMain.lua
+++ src/the_law_of_infinite_genes/Script/Blueprint/Prefabs/UI/Lobby/Store/StoreMain.lua
@@ -1,18 +1,17 @@
 ---@class StoreMain_C:UUserWidget
 ---@field BackpackList ReuseList2_C
+---@field Button_0 UButton
 ---@field EquipSlotList ReuseList2_C
----@field Image_2 UImage
----@field Image_3 UImage
----@field Image_4 UImage
 ---@field PorpertyList ReuseList2_C
 ---@field StoreBackpackTabList ReuseList2_C
 ---@field StoreRefinedPanel StoreRefinedPanel_C
----@field StoreResultantPanel StoreResultantPanel_C
 ---@field StoreStrengthenPanel StoreStrengthenPanel_C
 ---@field StoreTabList ReuseList2_C
----@field StoreToolBar StoreToolBar_C
 ---@field WidgetSwitcher_0 UWidgetSwitcher
 --Edit Below--
+if UGCGameSystem ~= nil and UGCGameSystem.UGCRequire ~= nil then
+end
+
 local StoreMain = { 
     bInitDoOnce = false,
     TabSelectIndex = 0,
@@ -22,6 +21,13 @@ local StoreMain = {
     StoreBackpackTabSelectIndex = 0,
     StoreChoseItemIndex = 0,
     } 
+
+function StoreMain:GetBackpackUseList()
+    if UGCItemManager ~= nil and UGCItemManager.UseList ~= nil and UGCItemManager.UseList.Backpack ~= nil then
+        return UGCItemManager.UseList.Backpack;
+    end
+    return "BackpackList";
+end
 
 function StoreMain:Construct()
     self:LuaInit();
@@ -37,11 +43,16 @@ function StoreMain:Tick(MyGeometry, InDeltaTime)
     end
     if self.BackpackSelectIndex ~= StoreManager.BackpackSelectIndex then
         self.BackpackSelectIndex = StoreManager.BackpackSelectIndex;
-        self.BackpackList:Reload(100);
+        self:SyncBackpackSelectIndex();
+        self:ReloadBackpackList();
     end
     if self.StoreBackpackTabSelectIndex ~= StoreManager.StoreBackpackTabSelectIndex then
         self.StoreBackpackTabSelectIndex = StoreManager.StoreBackpackTabSelectIndex;
         self.StoreBackpackTabList:Reload(4);
+        StoreManager.BackpackSelectIndex = -1;
+        self.BackpackSelectIndex = -1;
+        self:ClearBackpackSelectIndex();
+        self:ReloadBackpackList();
     end
 end
 
@@ -67,9 +78,77 @@ end
 
 function StoreMain:InitUI()
     self.StoreTabList:Reload(#self.TabLabelList);
-    self.BackpackList:Reload(100);
+    self:ReloadBackpackList();
     self.EquipSlotList:Reload(5);
     self.StoreBackpackTabList:Reload(#Config.ItemType);
+end
+
+function StoreMain:GetBackpackReloadCount()
+    if StoreManager ~= nil and StoreManager.MaxStoreBackpackSize ~= nil then
+        return StoreManager.MaxStoreBackpackSize;
+    end
+    return 100;
+end
+
+function StoreMain:GetBackpackTabIndex()
+    if StoreManager == nil or StoreManager.StoreBackpackTabSelectIndex == nil then
+        return 1;
+    end
+    return StoreManager.StoreBackpackTabSelectIndex + 1;
+end
+
+function StoreMain:GetBackpackDataList()
+    if StoreManager == nil or StoreManager.BackpackList == nil then
+        return {};
+    end
+
+    return StoreManager.BackpackList[self:GetBackpackTabIndex()] or {};
+end
+
+function StoreMain:GetBackpackItemData(Index)
+    local DataList = self:GetBackpackDataList();
+    if DataList == nil or Index == nil then
+        return nil;
+    end
+
+    return DataList[Index + 1] or DataList[Index];
+end
+
+function StoreMain:SyncBackpackListData()
+    if UGCItemManager == nil then
+        return;
+    end
+
+    local UseList = self:GetBackpackUseList();
+    UGCItemManager:RegisterList(UseList, self, self.BackpackList, self:GetBackpackReloadCount());
+    UGCItemManager:SetListData(UseList, self:GetBackpackDataList());
+end
+
+function StoreMain:SyncBackpackSelectIndex()
+    if UGCItemManager == nil then
+        return;
+    end
+
+    local UseList = self:GetBackpackUseList();
+    if StoreManager.BackpackSelectIndex == nil or StoreManager.BackpackSelectIndex < 0 then
+        UGCItemManager:ClearSelectedIndex(UseList);
+    else
+        UGCItemManager:SetSelectedIndex(UseList, StoreManager.BackpackSelectIndex);
+    end
+end
+
+function StoreMain:ClearBackpackSelectIndex()
+    if UGCItemManager ~= nil then
+        UGCItemManager:ClearSelectedIndex(self:GetBackpackUseList());
+    end
+end
+
+function StoreMain:ReloadBackpackList()
+    self:SyncBackpackListData();
+    self:SyncBackpackSelectIndex();
+    if self.BackpackList ~= nil then
+        self.BackpackList:Reload(self:GetBackpackReloadCount());
+    end
 end
 
 function StoreMain:StoreTabListUpdate(Item, Index)
@@ -86,12 +165,54 @@ function StoreMain:StoreTabListUpdate(Item, Index)
 end
 
 function StoreMain:BackpackListUpdate(Item, Index)
-    Item.Index = Index
-    if self.BackpackSelectIndex == Index then
-        Item:SetSelectedVisible(ESlateVisibility.Visible);
+    local UseList = self:GetBackpackUseList();
+    local ItemData = self:GetBackpackItemData(Index);
+
+    self:SyncBackpackListData();
+
+    if UGCItemManager ~= nil then
+        UGCItemManager:BindItem(Item, self, UseList, Index, ItemData);
+        return;
+    end
+
+    if Item.SetListContext ~= nil then
+        Item:SetListContext(self, UseList, Index);
     else
-        Item:SetSelectedVisible(ESlateVisibility.Collapsed);
-    
+        Item.OwnerWidget = self;
+        Item.UseListFlag = UseList;
+        Item.Index = Index;
+        Item.index = Index;
+    end
+    if ItemData ~= nil and Item.SetItemData ~= nil then
+        Item:SetItemData(ItemData);
+    elseif Item.SetNoneItem ~= nil then
+        Item:SetNoneItem();
+    end
+    if Item.SetSelectedVisible ~= nil then
+        Item:SetSelectedVisible(StoreManager.BackpackSelectIndex == Index);
+    end
+end
+
+function StoreMain:OnUGCItemClicked(UseListFlag, Index, ItemData, ItemWidget)
+    if UseListFlag == self:GetBackpackUseList() then
+        if ItemData == nil then
+            StoreManager.BackpackSelectIndex = -1;
+            self.BackpackSelectIndex = -1;
+            self:ClearBackpackSelectIndex();
+            return;
+        end
+
+        local bSameItem = StoreManager.BackpackSelectIndex == Index;
+        StoreManager.BackpackSelectIndex = Index;
+        self.BackpackSelectIndex = Index;
+
+        if UGCItemManager ~= nil then
+            UGCItemManager:SetSelectedIndex(UseListFlag, Index);
+        end
+
+        if bSameItem and StoreManager.OpenStoreItemInfoDialog ~= nil then
+            StoreManager:OpenStoreItemInfoDialog();
+        end
     end
 end
 
--- src/the_law_of_infinite_genes/Script/Blueprint/Prefabs/UI/Lobby/StoreManager.lua
+++ src/the_law_of_infinite_genes/Script/Blueprint/Prefabs/UI/Lobby/StoreManager.lua
@@ -9,6 +9,59 @@ StoreManager = StoreManager or
     StoreBackpackTabSelectIndex = 0;
     StoreStrengthenChoseItemIndex = 0;
     StoreRefinedChoseItemIndex = 0;
+    BackpackList = {
+    [1] = {
+        {
+            ItemId = 8310021,
+            Count = 1,
+            Quality = 3,
+            StrengthenLevel = 2,
+            RefineAttrs = {
+                { AttrId = 1, Value = 10 },
+                { AttrId = 2, Value = 5 },
+                { AttrId = 3, Value = 3 },
+                { AttrId = 4, Value = 1 },
+            },
+        },
+        {
+            ItemId = 8310026,
+            Count = 1,
+            Quality = 4,
+            StrengthenLevel = 5,
+            RefineAttrs = {
+                { AttrId = 1, Value = 18 },
+                { AttrId = 2, Value = 8 },
+                { AttrId = 3, Value = 6 },
+                { AttrId = 4, Value = 2 },
+            },
+        },
+        {
+            ItemId = 8310031,
+            Count = 1,
+            Quality = 5,
+            StrengthenLevel = 8,
+            RefineAttrs = {
+                { AttrId = 1, Value = 25 },
+                { AttrId = 2, Value = 12 },
+                { AttrId = 3, Value = 9 },
+                { AttrId = 4, Value = 4 },
+            },
+        },
+    },
+
+    [2] = {
+    },
+
+    [3] = {
+        { ItemId = 8310014, Count = 12, Quality = 1 },
+        { ItemId = 8310015, Count = 28, Quality = 1 },
+        { ItemId = 8310016, Count = 9, Quality = 2 },
+        { ItemId = 8310017, Count = 36, Quality = 1 },
+    },
+
+    [4] = {
+    },
+}
 }
 
 function StoreManager:RegisterComponentClass(CompClass)
