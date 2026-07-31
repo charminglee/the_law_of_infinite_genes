782315c7ba87f84d841fa49f4d6d722114827e6e
TEFuc0h1 <14045800+TEFuc0h1@user.noreply.gitee.com>
2026-07-31 18:58:15 +0800

    加入关卡结算界面与复活界面纹理并应用

---
+++ src/the_law_of_infinite_genes/Script/Blueprint/Prefabs/UI/Game/Breakthrough/Breakthrough_Result_UIBP.lua
@@ -0,0 +1,36 @@
+---@class Breakthrough_Result_UIBP_C:UUserWidget
+---@field BattleDetail UCanvasPanel
+---@field Button_Back UButton
+---@field Button_Share UButton
+---@field CanvasPanel_DataList UCanvasPanel
+---@field HorizontalBox_2 UHorizontalBox
+---@field Image_Logo UImage
+---@field Rank UCanvasPanel
+---@field Text_BackNumber UTextBlock
+---@field TextBlock_0 UTextBlock
+---@field TextBlock_Difficulty UTextBlock
+---@field TextBlock_GameMode UTextBlock
+---@field TextBlock_Result UTextBlock
+---@field TextBlock_UnlockMap UTextBlock
+---@field TextBlock_UnlockTitle UTextBlock
+---@field UGC_ReuseList2_Player UGC_ReuseList2_C
+---@field WaitingNode UCanvasPanel
+---@field WidgetSwitcher_Rank UWidgetSwitcher
+--Edit Below--
+local Breakthrough_Result_UIBP = { bInitDoOnce = false } 
+
+--[==[ Construct
+function Breakthrough_Result_UIBP:Construct()
+	
+end
+-- Construct ]==]
+
+-- function Breakthrough_Result_UIBP:Tick(MyGeometry, InDeltaTime)
+
+-- end
+
+-- function Breakthrough_Result_UIBP:Destruct()
+
+-- end
+
+return Breakthrough_Result_UIBP
