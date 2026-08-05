896e830112884907bc0dc3020f49a7dce7d76e34
cc <3331057238@qq.com>
2026-08-06 06:35:45 +0800

    Revert "战斗回合界面"
    This reverts commit 8379ef67

---
+++ src/the_law_of_infinite_genes/Script/Blueprint/Prefabs/UI/RaidInstance/RaidInstanceTop.lua
@@ -0,0 +1,10 @@
+---@class RaidInstanceTop_C:UAEUserWidget
+---@field Image_35 UImage
+---@field Image_36 UImage
+---@field Image_37 UImage
+---@field Image_38 UImage
+---@field Right UCanvasPanel
+--Edit Below--
+local RaidInstanceTop = { bInitDoOnce = false } 
+
+return RaidInstanceTop
