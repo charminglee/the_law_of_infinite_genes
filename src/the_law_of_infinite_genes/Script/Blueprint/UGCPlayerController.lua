c2a7ed05c86bfc253be23530645a5838d26a35df
TEFuc0h1 <14045800+TEFuc0h1@user.noreply.gitee.com>
2026-08-08 00:24:09 +0800

    修复PlayerController截断问题

--- src/the_law_of_infinite_genes/Script/Blueprint/UGCPlayerController.lua
+++ src/the_law_of_infinite_genes/Script/Blueprint/UGCPlayerController.lua
@@ -88,7 +88,7 @@ function UGCPlayerController:ReceiveBeginPlay()
             end
         end)
 
-        if GameState.IsInLobby() then
+        if UGCGameSystem.GameState.IsInLobby() then
             self:HandleBeginPlayInServerForLobby()
         else
             self:HandleBeginPlayInServerForFighting()
@@ -96,7 +96,7 @@ function UGCPlayerController:ReceiveBeginPlay()
     else
         LocalPlayerController = self ---@type UGCPlayerController_C
 
-        if GameState.IsInLobby() then
+        if UGCGameSystem.GameState.IsInLobby() then
             self:HandleBeginPlayInClientForLobby()
         else
             self:HandleBeginPlayInClientForFighting()
@@ -132,7 +132,8 @@ function UGCPlayerController:HandleBeginPlayInServerForFighting()
     print("[UGCPlayerController] HandleBeginPlayInServerForFighting "..#ld)
 end
 
-function UGCPlayerController:HandleBeginPlayInClientForLobby()    
+function UGCPlayerController:HandleBeginPlayInClientForLobby()
+    UGCLog.Log('HandleBeginPlayInClientForLobby')
     LobbyFlow:Go(LobbyFlowState.LFS_Lobby)
     local NewIndex = TimingListUtils.NewList()
     TimingListUtils.Add(NewIndex, 0, self, "GamePartReady")
