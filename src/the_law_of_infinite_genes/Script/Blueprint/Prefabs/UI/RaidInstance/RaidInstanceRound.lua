177ee90b29653d3b103fd5cc7c9f1611e7feff73
cc <3331057238@qq.com>
2026-09-03 22:35:56 +0800

    战斗界面HUD信息窗口

---
+++ src/the_law_of_infinite_genes/Script/Blueprint/Prefabs/UI/RaidInstance/RaidInstanceRound.lua
@@ -0,0 +1,13 @@
+---@class RaidInstanceRound_C:UAEUserWidget
+--Edit Below--
+local RaidInstanceRound = { bInitDoOnce = false } 
+--[==[ Construct
+function RaidInstanceRound:Construct()
+	
+end
+-- Construct ]==]
+-- function RaidInstanceRound:Tick(MyGeometry, InDeltaTime)
+-- end
+-- function RaidInstanceRound:Destruct()
+-- end
+return RaidInstanceRound
---
+++ src/the_law_of_infinite_genes/Script/Blueprint/Prefabs/UI/RaidInstance/RaidInstanceTopBg.lua
@@ -0,0 +1,27 @@
+---@class RaidInstanceTopBg_C:UAEUserWidget
+---@field Attr1 UTextBlock
+---@field Attr2 UTextBlock
+---@field Attr3 UTextBlock
+---@field Image_2 UImage
+---@field Image_3 UImage
+---@field Image_12 UImage
+---@field Image_13 UImage
+---@field Image_14 UImage
+--Edit Below--
+local RaidInstanceTopBg = { bInitDoOnce = false } 
+
+--[==[ Construct
+function RaidInstanceTopBg:Construct()
+	
+end
+-- Construct ]==]
+
+-- function RaidInstanceTopBg:Tick(MyGeometry, InDeltaTime)
+
+-- end
+
+-- function RaidInstanceTopBg:Destruct()
+
+-- end
+
+return RaidInstanceTopBg
